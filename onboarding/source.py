ade3b9ed617bb9b8563ccc09f51463297fc79ab5
Mykhailo Tsysin <misha.tsysin@gmail.com>
2023-01-17 16:11:08 -0500

    converted to class

--- onboarding/source.py
+++ onboarding/source.py
@@ -4,35 +4,48 @@ import cv2
 import matplotlib.pyplot as plt
 
 cv2.destroyAllWindows()
-# Load sample image and perform simple operation
-img = cv2.imread("test.jpeg", cv2.IMREAD_COLOR)
-
-# Show image
-def display(img):
-    cv2.imshow("image", img)
-    cv2.waitKey(0)
-
-# try converting to grayscale
-# basic info
-print(img.shape)
-print(type(img))
 
 # BT.709
 # BT.601
 # R, G, B
-coefs_709 = [0.2126, 0.7152, 0.0722]
-coefs_601 = [0.2126, 0.7152, 0.0722]
-coefs_uniform = [0.33, 0.33, 0.33]
+COEFS_709 = [0.2126, 0.7152, 0.0722]
+COEFS_601 = [0.2126, 0.7152, 0.0722]
+COEFS_UNIFORM = [0.33, 0.33, 0.33]
+BLUR_FILT = np.array(
+    [[1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25],
+    [1/25, 1/25, 1/25, 1/25, 1/25]]
+)
 
-def convert_to_grayscale(img, coefs):
-    coefs = np.array(coefs)
-    return (img@coefs).astype(np.uint8)
+# SOBEL_X = np.array(
+#     [[2, 2, 4, 2, 2],
+#     [1, 1, 2, 1, 1],
+#     [0, 0, 0, 0, 0],
+#     [-1, -1, -2, -1, -1],
+#     [-2, -2, -4, -2, -2]]
+# )
 
-img1 = convert_to_grayscale(img, coefs=coefs_709)
-img2 = convert_to_grayscale(img, coefs=coefs_601)
-img3 = convert_to_grayscale(img, coefs=coefs_uniform)
+# SOBEL_Y = np.array(
+#     [[2, 1, 0, -1, -2],
+#     [2, 1, 0, -1, -2],
+#     [4, 2, 0, -2, -4],
+#     [2, 1, 0, -1, -2],
+#     [2, 1, 0, -1, -2]]
+# )
 
-img_grayscale = img1
+SOBEL_X = np.array(
+    [[1, 2, 1],
+    [0, 0, 0],
+    [-1, -2, -1]]
+)
+
+SOBEL_Y = np.array(
+    [[1, 0, -1],
+    [2, 0, -2],
+    [1, 0, -1]]
+)
 
 # print(img1.shape)
 # print(img)
@@ -45,66 +58,100 @@ img_grayscale = img1
 # Test filters:
 # Custom filter application for grayscale images:
 # works only for widows with odd size
-def apply_filter_zero_padding(filter, img):
-    window_size = filter.shape[0]
-    padding = window_size//2
-    img_size = img.shape
-    # Pad the image:
-    corner_padding = np.zeros((padding, padding))
-    horizontal_padding = np.zeros((padding, img_size[1]))
-    vertical_padding = np.zeros((img_size[0], padding))
+class Image:
+    def __init__(self, path = None, img = None) -> None:
+        # Load sample image to perform simple operations
+        if path:
+            self.img = cv2.imread(path, cv2.IMREAD_COLOR)
+        else:
+            self.img = img
+        self.size = self.img.shape
 
-    img = np.concatenate([
-            np.concatenate([corner_padding, horizontal_padding, corner_padding], axis=1),
-            np.concatenate([vertical_padding, img, vertical_padding], axis=1),
-            np.concatenate([corner_padding, horizontal_padding, corner_padding], axis=1),
-        ],
-        axis=0
-    )
+    def display(self):
+        # Show image
+        cv2.imshow("image", self.img)
+        cv2.waitKey(0)
 
-    img_filt = np.zeros(img_size)
-    # apply filter
-    for i in range(img_size[0]):
-        for j in range(img_size[1]):
-            img_filt[i][j] = np.sum(img[i:i+window_size,j:j+window_size] * filter)
+    def convert_to_grayscale(self, coefs):
+        coefs = np.array(coefs)
+        return Image(img = (self.img@coefs).astype(np.uint8))
+
+    def apply_filter_zero_padding(self, filter):
+        window_size = filter.shape[0]
+        padding = window_size//2
+        img_size = self.size
+        # Pad the image:
+        corner_padding = np.zeros((padding, padding))
+        horizontal_padding = np.zeros((padding, img_size[1]))
+        vertical_padding = np.zeros((img_size[0], padding))
+
+        img_temp = np.concatenate([
+                np.concatenate([corner_padding, horizontal_padding, corner_padding], axis=1),
+                np.concatenate([vertical_padding, self.img, vertical_padding], axis=1),
+                np.concatenate([corner_padding, horizontal_padding, corner_padding], axis=1),
+            ],
+            axis=0
+        )
+
+        img_filt = np.zeros(img_size)
+        # apply filter
+        for i in range(img_size[0]):
+            for j in range(img_size[1]):
+                img_filt[i][j] = np.sum(img_temp[i:i+window_size,j:j+window_size] * filter)
+        
+        return Image(img = img_filt.astype(np.uint8))
+
+    # Show image histogram
+    def show_histogram(self, bin_size):
+        hist = [0] * (256//bin_size)
+        for row in self.img:
+            for pixel in row:
+                hist[pixel//bin_size] += 1
+        plt.bar([i*bin_size for i in range(len(hist))], hist)
+        plt.show()
+
+    def invert(self):
+        return Image(img = 255 - self.img)
+
+    def threshold(self, threshold):
+        return Image(img = (self.img > threshold).astype(np.uint8)*255)
+
+    def sobel_edge(self):
+        x = self.apply_filter_zero_padding(SOBEL_X)
+        y = self.apply_filter_zero_padding(SOBEL_Y)
+        return Image(img = np.sqrt(np.square(x.img) + np.square(y.img)).astype(np.uint8))
     
-    return img_filt.astype(np.uint8)
-    # print(img)
+image = Image(path="test.jpeg")
 
-# Show image histogram
-def img_histogram(img, bin_size):
-    hist = [0] * (256//bin_size)
-    for row in img:
-        for pixel in row:
-            hist[pixel//bin_size] += 1
-    plt.bar([i*bin_size for i in range(len(hist))], hist)
-    plt.show()
+# Compare grayscale conversion
+# for coefs in [COEFS_601, COEFS_709, COEFS_UNIFORM]:
+#     image.convert_to_grayscale(coefs).display()
 
-def invert_img(img):
-    return 255 - img
-    
+image = image.convert_to_grayscale(COEFS_709)
 
-blur_filter = np.array(
-    [[1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25]]
-)
+# Try blurred image
+blurred_image = image.apply_filter_zero_padding(filter=BLUR_FILT)
+# blurred_image.display()
 
-# blurred_img = apply_filter_zero_padding(filter=blur_filter, img=img_grayscale)
+# Try to show image histogram
+# image.show_histogram(2)
 
-# display(blurred_img)
-# img_histogram(img_grayscale, 2)
-# display(invert_img(img_grayscale))
+# Invert image
+# image.invert().display()
 
-blur_filter = np.array(
-    [[1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25],
-    [1/25, 1/25, 1/25, 1/25, 1/25]]
-)
+# Threhold image
+# image.threshold(100).display()
+
+# Edge detection
+# image.sobel_edge().display()
+i = image.apply_filter_zero_padding(filter=SOBEL_X)
+i.display()
+
+i = image.apply_filter_zero_padding(filter=SOBEL_Y)
+i.display()
+
+
+
 
 cv2.destroyAllWindows()
 
